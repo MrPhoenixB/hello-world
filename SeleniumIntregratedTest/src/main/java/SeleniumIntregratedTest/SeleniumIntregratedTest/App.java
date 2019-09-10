1edac09e157498bd942fa7f725111d20ef83e999
Tanmoy <Tanmoy@DESKTOP-8LVOAOH>
2019-09-11 00:18:05 +0530

    First Commit

---
+++ SeleniumIntregratedTest/src/main/java/SeleniumIntregratedTest/SeleniumIntregratedTest/App.java
@@ -0,0 +1,13 @@
+package SeleniumIntregratedTest.SeleniumIntregratedTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
