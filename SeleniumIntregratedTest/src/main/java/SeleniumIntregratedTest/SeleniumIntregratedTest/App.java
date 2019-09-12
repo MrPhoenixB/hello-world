a48d784d36b00d46fda175b2addcdd07757ac276
MrPhoenixB <tanmoybiswas96@gmail.com>
2019-09-12 19:50:27 +0530

    copied test t main

--- SeleniumIntregratedTest/src/main/java/SeleniumIntregratedTest/SeleniumIntregratedTest/App.java
+++ SeleniumIntregratedTest/src/main/java/SeleniumIntregratedTest/SeleniumIntregratedTest/App.java
@@ -1,13 +1,46 @@
 package SeleniumIntregratedTest.SeleniumIntregratedTest;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.DriverManagerType;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 /**
  * Hello world!
- *
+ * https://www.facebook.com
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static WebDriver driver=null;
+
+
+	@Before
+	public void setUp() throws Exception {
+		
+		WebDriverManager.getInstance(DriverManagerType.CHROME).setup();
+		driver=new ChromeDriver();
+		
+		
+	}
+		
+	
+	@Test
+	public void demoTest() {
+		
+		driver.get("https://www.facebook.com");
+		
+		
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		
+		System.out.println("Test Ended");
+		
+	}
 }
